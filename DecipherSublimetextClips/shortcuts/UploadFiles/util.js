6d5654452601a5fb009593e28a431f18f9cbe50a
Chan Lee <dlcks17@kakao.com>
2023-05-22 10:48:00 +0900

    toggle arrow update

--- DecipherSublimetextClips/shortcuts/UploadFiles/util.js
+++ DecipherSublimetextClips/shortcuts/UploadFiles/util.js
@@ -962,9 +962,17 @@ function groupToggleSetting(){
     groupSelected();
     document.querySelector('.answers').addEventListener('click', groupSelected);
 
+    const toggleArrow = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg>`;
+
     groupToggle.forEach((group) => {
         const groupRowCell = group.querySelector('.ch-group-rows');
         const groupName = group.querySelector('.ch-group-name');
+
+        const arrowDiv = document.createElement('div');
+        arrowDiv.classList.add('ch-group-arrow');
+        arrowDiv.innerHTML = toggleArrow;
+
+        groupName.appendChild(arrowDiv);
 
         const clickHandler = () => maxHeightHandler(groupRowCell, groupName);
         clickHandler();
